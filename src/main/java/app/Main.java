526da1fee4cfa6a0a50ecadbfa0aed549d6236f0
Admin <melnukvikysa@gmail.com>
2024-09-27 17:03:35 +0300

    info about project

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Програма для конвертації миль в кілометри, кілометрів в милі");
+        }
+}
