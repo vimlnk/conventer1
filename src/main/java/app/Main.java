ccabff9ff71c6d607510a976bc18bd2c6f0573a5
Admin <melnukvikysa@gmail.com>
2024-09-27 17:29:54 +0300

    miles to km conversion

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,21 @@
 package app;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Програма для конвертації миль в кілометри, кілометрів в милі");
+
+        System.out.println("Перетворюємо милі в кілометри ");
+        System.out.println("Введіть милі : ");
+        Scanner scanner = new Scanner(System.in);
+        double mi = scanner.nextInt();
+        double km;
+        km = mi*1.60934;
+        System.out.println(mi + " миль "+ "= "+ km +" кілометрів. ");
+
+
+
         }
 }
